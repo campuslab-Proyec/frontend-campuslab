1c4f8f26df4bb11854b601327ead42ce9d02b90a
estefanno monar <estefannomonarsoto2004@gmail.com>
2026-09-14 20:53:02 -0300

    modificaion de Dockerfile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: true,
   msalConfig: {
     auth: {
-      clientId: '718a6ef2-45a4-4f06-84c9-6f54de8c3514', 
-      authority: 'https://login.microsoftonline.com/aff2eb38-d6cf-4220-8527-53f1cb6aed15', 
-      redirectUri: 'http://localhost:4200' 
-    }
+      clientId: '718a6ef2-45a4-4f06-84c9-6f54de8c3514',
+      authority: 'https://login.microsoftonline.com/aff2eb38-d6cf-4220-8527-53f1cb6aed15',
+      redirectUri: 'http://<TU-IP-O-DOMINIO-EC2>',
+    },
   },
   apiConfig: {
     scopes: ['api://8d19bb45-1842-43ce-b76c-4513b979df81/access_as_user'],
-    uri: 'http://localhost:8080/api/bff/' 
+    uri: 'http://<TU-IP-O-DOMINIO-EC2>:8080/api/bff/',
   },
-  bffUrl: 'http://localhost:8080/api/bff'
+  bffUrl: 'http://<TU-IP-O-DOMINIO-EC2>:8080/api/bff',
 };
