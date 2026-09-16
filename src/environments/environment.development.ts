21c8c97d84c9c4ebf545f8dd5f87f0013093df25
ink616 <taylormedolphe@gmail.com>
2026-09-16 06:19:32 -0300

    feat: integracion de BFF con Azure AD MSAL, controladores de microservicios y correccion de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,15 +1,26 @@
-export const environment = {
-  production: false,
-  msalConfig: {
-    auth: {
-        clientId: '718a6ef2-45a4-4f06-84c9-6f54de8c3514', 
-        authority: 'https://login.microsoftonline.com/aff2eb38-d6cf-4220-8527-53f1cb6aed15', 
-        redirectUri: 'http://localhost:4200' 
+import { Configuration, LogLevel } from '@azure/msal-browser';
+
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: '718a6ef2-45a4-4f06-84c9-6f54de8c3514',
+    authority: 'https://login.microsoftonline.com/aff2eb38-d6cf-4220-8527-53f1cb6aed15',
+    redirectUri: 'http://localhost/',
+    postLogoutRedirectUri: 'http://localhost/'
+  },
+  cache: {
+    cacheLocation: 'localStorage'
+  },
+  system: {
+    loggerOptions: {
+      logLevel: LogLevel.Info,
+      piiLoggingEnabled: false
     }
-  },
-  apiConfig: {
-    scopes: ['api://8d19bb45-1842-43ce-b76c-4513b979df81/access_as_user'],
-    uri: 'http://localhost:8080/api/bff/' 
-  },
-  bffUrl: 'http://localhost:8080/api/bff'
+  }
+};
+
+export const protectedResources = {
+  bffApi: {
+    endpoint: 'http://localhost:8080/api/bff/',
+    scopes: ['api://718a6ef2-45a4-4f06-84c9-6f54de8c3514/access_as_user']
+  }
 };
